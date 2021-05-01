4710d869cf40f57d63e3520398de50eb4547f0e5
StanislavEng <ictstas@gmail.com>
2021-05-01 11:18:51 -0700

    Adding some initial work

--- Les1.py
+++ Les1.py
@@ -2,7 +2,7 @@ import cv2 as cv # importing openCV and giving it variable name cv
 import sys       # importing system commands
 import numpy
 
-#cv.samples.addSamplesDataSearchPath("/home/pi/opencv/samples/data")
+cv.samples.addSamplesDataSearchPath("/home/pi/opencv/samples/data")
 #img = cv.imread(cv.samples.findFile("squares.jpg"))
 img = cv.imread(cv.samples.findFile("starry_night.jpg"))
 
